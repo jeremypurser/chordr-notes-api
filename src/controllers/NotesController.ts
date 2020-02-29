017df85ea2fd659a8245a2faee3ae66c5f54670a
Jeremy Purser <jeremypurser@gmail.com>
2020-02-29 16:23:35 -0500

    Remove unused imports, variables

--- src/controllers/NotesController.ts
+++ src/controllers/NotesController.ts
@@ -1,9 +1,5 @@
 import { Request, Response } from 'express';
-import { Pool } from 'pg';
-import { connection } from '../../config';
 import NotesModel from '../models';
-
-const pool = new Pool(connection);
 
 export const postNote = (req: Request, res: Response) => {
   NotesModel.post(req)
