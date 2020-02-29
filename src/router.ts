25ebd69c02391ebcb93ee5277245d2882374d8df
Jeremy Purser <jeremypurser@gmail.com>
2020-02-29 16:23:55 -0500

    Use cors to allow client app

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import parser from 'body-parser';
+import cors from 'cors';
 import { postNote } from './controllers';
 
 const app = express();
 
+app.use(cors({ origin: 'http://localhost:3000/' }));
 app.use(parser.json());
 
 app.post('/notes', postNote);
