772380584dfcca37e6c324179eb6d2296ff35c01
Jeremy Purser <jeremypurser@gmail.com>
2020-03-01 11:41:52 -0500

    Add get route

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,16 @@
-import express from 'express';
 import parser from 'body-parser';
 import cors from 'cors';
-import { postNote } from './controllers';
+import express from 'express';
+import NotesController from './controllers';
+
+const { postNote, getNotes } = NotesController;
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000/' }));
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(parser.json());
 
+app.get('/notes/:id', getNotes);
 app.post('/notes', postNote);
 
 export default app;
