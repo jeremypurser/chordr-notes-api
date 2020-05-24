0c73c6d78252cd6d018eaf2a6664930f0f2a9e3b
Jeremy Purser <jeremypurser@gmail.com>
2020-05-24 13:36:19 -0400

    Correct typing on delete

--- src/core/Entities/Chord.ts
+++ src/core/Entities/Chord.ts
@@ -5,7 +5,5 @@ export interface NotesController {
   getNoteById(id: string): Promise<ChordrResponse<Chord, undefined>>;
   getAllNotes(userId: string): Promise<ChordrResponse<Chord[], undefined>>;
   updateNote(note: Chord): Promise<ChordrResponse<Chord, 'updated'>>;
-  deleteNote(
-    note: Chord
-  ): Promise<ChordrResponse<Chord, 'marked for deletion'>>;
+  deleteNote(id: string): Promise<ChordrResponse<Chord, 'marked for deletion'>>;
 }
