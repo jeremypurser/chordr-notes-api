b88de2335f530fad041d33cedd0d4e8c51edc581
Jeremy Purser <jeremypurser@gmail.com>
2020-05-24 16:16:19 -0400

    Implement and use express adapter

--- src/core/Adapters/RequestAdapter.ts
+++ src/core/Adapters/RequestAdapter.ts
@@ -1,1 +1,40 @@
-export function expressReqAdapter() {}
+import { Request, Response } from 'express';
+import { ChordrResponse } from '../Entities';
+
+export function expressAdapter(
+  controller: (a: any) => Promise<ChordrResponse<any, any>>
+) {
+  return function (req: Request, res: Response) {
+    switch (req.method) {
+      case 'POST':
+        controller(req.body).then(data => {
+          res.status(data.success ? 201 : 500).json(data);
+        });
+        break;
+      case 'GET':
+        if (req.params.userId) {
+          controller(req.params.userId).then(data => {
+            res.status(data.success ? 200 : 500).json(data);
+          });
+        } else if (req.params.id) {
+          controller(req.params.id).then(data => {
+            res.status(data.success ? 200 : 500).json(data);
+          });
+        }
+        break;
+      case 'PUT':
+        controller({ id: req.params.id, ...req.body }).then(data => {
+          res.status(data.success ? 204 : 500).json(data);
+        });
+        break;
+      case 'DELETE':
+        controller(req.params.id).then(data => {
+          res.status(data.success ? 204 : 500).json(data);
+        });
+        break;
+      default:
+        throw new Error('Invalid request');
+        break;
+    }
+  };
+}
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,8 @@
 import parser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
-import NotesController from './controllers';
-
-const { postNote, getNotes, editNote, deleteNote } = NotesController;
+import notesController from './controllers';
+import { expressAdapter } from './core/Adapters/RequestAdapter';
 
 const app = express();
 
@@ -12,13 +11,13 @@ app.use(parser.json());
 
 // TODO: change route include user_id
 // Create
-app.post('/notes', postNote);
+app.post('/notes', expressAdapter(notesController.addNote));
 // Read
-app.get('/:user_id/notes', getNotes);
+app.get('/:user_id/notes', expressAdapter(notesController.getAllNotes));
 // Update
-app.put('/notes/:note_id', editNote);
+app.put('/notes/:note_id', expressAdapter(notesController.updateNote));
 // Delete
-app.delete('/notes/:note_id', deleteNote);
+app.delete('/notes/:note_id', expressAdapter(notesController.deleteNote));
 
 // TODO: endpoint to delete all notes in dev
 
