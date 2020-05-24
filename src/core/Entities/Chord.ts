710ccbd09620977993eb7da474c94cd6df8129cb
Jeremy Purser <jeremypurser@gmail.com>
2020-05-24 15:12:55 -0400

    Move file

--- src/core/Entities/Chord.ts
+++
@@ -1,9 +0,0 @@
-import { Chord, ChordrResponse } from '.';
-
-export interface NotesController {
-  addNote(note: Exclude<Chord, 'id'>): Promise<ChordrResponse<Chord, 'posted'>>;
-  getNoteById(id: string): Promise<ChordrResponse<Chord, undefined>>;
-  getAllNotes(userId: string): Promise<ChordrResponse<Chord[], undefined>>;
-  updateNote(note: Chord): Promise<ChordrResponse<Chord, 'updated'>>;
-  deleteNote(id: string): Promise<ChordrResponse<Chord, 'marked for deletion'>>;
-}
