194815796f5ee0e9d39b52d946fac4ac9fed61a6
Jeremy Purser <jeremypurser@gmail.com>
2020-05-24 17:14:18 -0400

    Move routes and env vars into config dir

---
+++ src/config/routes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import notesController from '../controllers';
+import { expressAdapter } from '../core/Adapters/RequestAdapter';
+
+// base => /api/v1
+const router = Router();
+
+// Create
+router.post('/:userId/notes', expressAdapter(notesController.addNote));
+
+// Read all
+router.get('/:userId/notes', expressAdapter(notesController.getAllNotes));
+
+// Read 1
+router.get('/notes/:id', expressAdapter(notesController.getNoteById));
+
+// Update
+router.put('/notes/:id', expressAdapter(notesController.updateNote));
+
+// Delete
+router.delete('/notes/:id', expressAdapter(notesController.deleteNote));
+
+export default router;
